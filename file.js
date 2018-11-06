02c4b8e36e0efaefd4ce27c974316d98a398d248
Mcarpenter-ucvts <marky0515@gmail.com>
2018-11-06 13:23:12 -0500

    three buttons

---
+++ file.js
@@ -0,0 +1,14 @@
+function sayHello () {
+  //var p = document.getElementById('hello')
+  //p.innerHTML = "Hello, World!";
+  console.log("Hello, World!");
+}
+
+function popUP () {
+  alert("Hello, World!");
+}
+
+function Browser () {
+  var p = document.getElementById('Hello');
+  p.innerHTML = "Hello, World!";
+}
